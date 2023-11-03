57396841944132f94f622944e00e28d861e34594
Gabriel Daramola <gabamazing21@gmail.com>
2023-11-03 03:43:55 +0100

    python module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
 if (__name__ == '__main__'):
-    print(f"{len(argv) - 1} arguments.")
-    for i in range(1, len(argv)):
-        print(f"{i}: {argv[i]}")
+    arg_num = len(argv) - 1
+    if (arg_num < 1):
+        print(f"{arg_num} arguments.")
+    elif (arg_num == 1):
+        print(f"{arg_num} argument:")
+        print(f"{arg_num}: {argv[arg_num]}")
+    elif (arg_num > 1):
+        for i in range(1, len(argv)):
+            print(f"{arg_num} arguments:")
+            print(f"{i}: {argv[i]}")
