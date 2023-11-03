2e27844348ea4eda4e81892a54ec80e849cd6694
Gabriel Daramola <gabamazing21@gmail.com>
2023-11-03 02:20:00 +0100

    import modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if (__name__ == '__main__'):
     from add_0 import add
-    a = 1
-    b = 2
-    print("1 + 2 =", add(a, b))
+    a, b = 1, 2
+    print(f"1 + 2 = {add(a, b):d}")
