578241116eefa472cdb705a6a7d40a115a58fc2a
Gabriel Daramola <gabamazing21@gmail.com>
2023-11-06 23:59:50 +0100

    oka

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    sum_tuple_a = 0
-    sum_tuple_b = 0
-    if (len(tuple_a) == 0):
-        sum_tuple_a = 0
-    elif (len(tuple_a) < 2):
-        sum_tuple_a = tuple_a[0] + 0
-    else:
-        for index, i in enumerate(tuple_a):
-            if (index >= 2):
-                break
-            sum_tuple_a += i
-
-    if (len(tuple_b) == 0):
-        sum_tuple_b = 0
-    elif (len(tuple_b) < 2):
-        sum_tuple_b = tuple_b[0] + 0
-    else:
-        for index, i in enumerate(tuple_b):
-            if (index >= 2):
-                break
-            sum_tuple_b += i
-    return (sum_tuple_a, sum_tuple_b)
+    i_0 = 0
+    i_1 = 0
+    i_a = len(tuple_a) - 1
+    i_b = len(tuple_b) - 1
+    if (len(tuple_a) >= 2 and len(tuple_b) >= 2):
+        i_0 = tuple_a[0] + tuple_b[0]
+        i_1 = tuple_a[1] + tuple_b[1]
+    elif (len(tuple_a) < 2 or len(tuple_b) < 2):
+        i_0 = (0 if (i_a < 0) else tuple_a[0]) + (0 if i_b < 0 else tuple_b[0])
+        i_1 = (0 if (i_a < 1) else tuple_a[1]) + (0 if i_b < 1 else tuple_b[1])
+    return (i_0, i_1)
