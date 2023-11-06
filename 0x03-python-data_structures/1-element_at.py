c7d331eed808314b0810dfeceaa453ef93e4a978
Gabriel Daramola <gabamazing21@gmail.com>
2023-11-06 03:22:16 +0100

    return element at index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+def element_at(my_list, idx):
+    if (idx < 0):
+        return "None"
+    for i in my_list:
+        if (i == idx):
+            return (my_list[i])
+            break
+    return "None"
