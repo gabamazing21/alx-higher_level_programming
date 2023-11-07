70f3f7723f905dfd5a32c0ecebf0b8012aca76ed
Gabriel Daramola <gabamazing21@gmail.com>
2023-11-07 01:20:05 +0100

    okay

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -7,4 +7,4 @@ def delete_at(my_list=[], idx=0):
         for index, j in enumerate(my_list):
             if (index == idx):
                 del my_list[index]
-                return my_list
+                return (my_list)
