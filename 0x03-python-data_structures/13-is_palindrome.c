7fd8fe13ef7b91301e5edd8f88f8866af306359f
Gabriel Daramola <gabamazing21@gmail.com>
2023-11-09 04:19:26 +0100

    list comprehension

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "list.h"
+int is_palindrome(listint_t **head)
+{
+	listint_t *h;
+	h = *head;
+	/* getting the length of the node */
+	int i = 0, j = 0, half = 0;
+	while (h->next != NULL)
+	{
+		h = h->next;
+		i++
+	};
+
+	for (j = 0; head->next ! NULL: j++)
+	{
+		half = i/2;
+		if (
+	};
+}
