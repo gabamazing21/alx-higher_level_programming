a16fd171a67a357d88187b8a144438ee288c93e9
Gabriel Daramola <gabamazing21@gmail.com>
2023-11-24 00:03:10 +0100

    safe to print

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if type(value) is int:
-        print("{:d}".format(value))
-        return True
-    else:
-        return False
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+        else:
+            return False
+    except Exception:
+        print("there's error")
