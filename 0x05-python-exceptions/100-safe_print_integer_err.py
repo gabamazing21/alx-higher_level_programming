f218eb0ed7b012693f99632826bc2d88d479cd5d
Gabriel Daramola <gabamazing21@gmail.com>
2023-11-26 13:36:04 +0100

    catch the exception with valueError

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
-        else:
-            return False
-    except Exception as err:
-        print(err)
-
+        print("{:d}".format(value))
+        return True
+    except ValueError as err:
+        print(err, file=sys.stderr)
+        return False
