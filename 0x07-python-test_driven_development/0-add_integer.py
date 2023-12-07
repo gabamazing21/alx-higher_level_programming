7783e4ca4a61fe2b0cea3e663a9f663562890431
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-07 21:14:24 +0100

    adding all test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module to add integer
+
+"""
+
+
+def add_integer(a, b=98):
+    try:
+        """
+        this function return a + b
+        """
+        if (type(a) is not int and type(a) is not float):
+            raise TypeError("a must be an integer")
+        elif (type(b) is not int and type(b) is not float):
+            raise TypeError("b must be an integer")
+        elif (a + 1 == a):
+            raise OverflowError("a is too large")
+        elif (b + 1 == b):
+            raise OverflowError("b is too large")
+        elif (a is None and b is not None):
+            raise SyntaxError("missing one argument")
+        elif (a is not None and b is None):
+            raise SyntaxError("missing one argument")
+        elif (a is None and b is None):
+            raise SyntaxError("missing two arguments")
+        else:
+            return int(a) + int(b)
+    except TypeError as e:
+        return str(e)
+    except OverflowError as e:
+        return str(e)
+    except SyntaxError as e:
+        return str(e)
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+True Module
+"""
+def is_kind_of_class(obj, a_class):
+    """
+    method return true or false
+    if obj is child or instance of
+    class
+    """
+    if (isinstance(obj, a_class) or issubclass(obj, a_class)):
+        return True
+    else:
+        return False
