7d2e85b722a508d5920335fd9937d49ff5b95e98
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-07 04:50:09 +0100

    working with instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module to
+check your instance
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    is_same_class return true or false
+    """
+    return isinstance(obj, a_class)
