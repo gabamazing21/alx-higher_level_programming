11fd70f0bf53a47f8febc76cb06c87c9777e7e4c
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-09 23:33:05 +0100

    working with these class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,13 +2,15 @@
 """
 True Module
 """
+
+
 def is_kind_of_class(obj, a_class):
     """
     method return true or false
     if obj is child or instance of
     class
     """
-    if (isinstance(obj, a_class) or issubclass(obj, a_class)):
+    if (isinstance(obj, a_class)):
         return True
     else:
         return False
