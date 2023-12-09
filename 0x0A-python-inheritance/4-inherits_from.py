7d6b81dd1db1cd6dc1709fb06720766a96deffb8
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-09 23:42:42 +0100

    checking for issubclass()

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module to check inheritance
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    this function
+    check if it's a subclass
+    """
+    if (issubclass(type(obj), a_class)):
+        return True
+    else:
+        return False
