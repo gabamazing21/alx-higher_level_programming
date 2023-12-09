977ed87ba3d802171f8fcf64d7ea23678af733d2
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-10 00:05:20 +0100

    checking for some method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Geometry module
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry
+    class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
