61dbec8945ed34a01899be05139f3c18687d2a18
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-10 16:38:45 +0100

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module helps to understand write/read and
+opening of file in python
+"""
+
+
+def read_file(filename=""):
+    """
+    read_file: it open file for reading
+    """
+    with open(filename, 'r', encoding="UTF-8") as f:
+        for line in f:
+            print(line)
