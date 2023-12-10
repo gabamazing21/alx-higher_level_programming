106b14726d22a50dfdc12ecdcc413ca5d99633d8
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-10 09:09:52 +0100

    i think we are okay

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,6 +15,12 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """integer_validator"""
+        if (name is None):
+            raise TypeError("missing one argument")
+        if (value is None):
+            raise TypeError("missing one argument")
+        if (value is None and name is None):
+            raise TypeError("missing two arguments")
         if (type(value) is not int):
             raise TypeError(f"{name} must be an integer")
         if (value <= 0):
