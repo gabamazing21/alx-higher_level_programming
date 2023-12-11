3392db383b8c68e166e6aaf831928a11eb91c009
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-11 06:30:50 +0100

    seriolize an object or javascript representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+"""
+this module
+we work with json file
+"""
+
+
+def to_json_string(my_obj):
+    """
+    to_json_string
+    conver to json string
+    """
+    return json.dumps(my_obj)
