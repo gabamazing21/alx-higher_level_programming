e6d6477462e63e697253421fa3bf22f0b5d19b5f
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-11 06:38:41 +0100

    we have give it a comment

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -8,7 +8,8 @@ we work with json file
 
 def to_json_string(my_obj):
     """
-    to_json_string
-    conver to json string
+    to_json_string - it json object
+    parameter: my_obj
+    it return json object
     """
     return json.dumps(my_obj)
