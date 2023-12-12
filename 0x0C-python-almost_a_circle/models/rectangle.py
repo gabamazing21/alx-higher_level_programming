1d06fac9f67e8f403c5b674868e20ab543f5c8ad
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-12 05:58:04 +0100

    k

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -134,11 +134,10 @@ class Rectangle(Base):
             if 'y' in kwargs:
                 self.__y = kwargs['y']
 
-    def to_dictionary(self):
-        """to_dictionary """
-       rect_dict =  {}
-       rect_dict['id'] = self.id
-       rect_dict['size'] = self.__size
-       rect_dict['x'] = self.__x
-       rect_dict['y'] = self.__y
-       return rect_dict
+        def to_dictionary(self):
+            """to_dictionary """
+            rect_dict =  {}rect_dict['id'] = self.id
+            rect_dict['size'] = self.__size
+            rect_dict['x'] = self.__x
+            rect_dict['y'] = self.__y
+            return rect_dict
