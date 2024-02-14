dcc87cf665c1aae810819335091445ae4b417d20
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-14 04:59:14 +0100

    task 2

--- 0x13-javascript_objects_scopes_closures/0-main.js
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/Users/gabriel/.nvm/versions/node/v14.0.0/bin/node
 const Rectangle = require('./0-rectangle');
 
 const r1 = new Rectangle();
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/Users/gabriel/.nvm/versions/node/v14.0.0/bin/node
+class Rectangle {
+    //empty class
+    constructor(w, h){
+        this.width = w;
+        this.height = h;
+    }
+}
