e2b2b7391bdac7f095ac8452a19f06826e1643ee
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-16 00:15:14 +0100

    okay

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const argv = process.argv.slice(2);
-if (argv.length > 0) {
+if (argv.length === 1) {
   console.log('Argument found');
 } else if (argv.length === 0) {
+  console.log('No argument');
+} else {
   console.log('No argument');
 }
