7686fdc118a992a50b292c98a91378680ae9ea83
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-15 05:43:28 +0100

    okay

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
-// #!/Users/gabriel/.nvm/versions/node/v14.21.3/bin/node
-const myVar = "JavaScript is amazing"
-console.log(myVar)
+#!/Users/gabriel/.nvm/versions/node/v14.21.3/bin/node
+// #!/usr/bin/node
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
