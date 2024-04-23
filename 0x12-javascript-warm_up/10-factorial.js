ee2dc8634192589536066af3826b277b24b38c74
gabriel <gabamazing21@gmaile.com>
2024-04-23 10:03:46 -0700

    fixed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const argv = process.argv.slice(2);
-console.log(factorial(Number.parseInt(argv[0])));
+if (argv.length === 0) {
+  console.log(1);
+} else {
+  console.log(factorial(Number.parseInt(argv[0])));
+}
 function factorial (x) {
   if (x === 0) {
     return (1);
