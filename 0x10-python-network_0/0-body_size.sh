6f16a44bd1a24078bad4e611eb0fcdc806d739aa
Gabriel Daramola <gabamazing21@gmail.com>
2024-07-07 10:33:50 +0100

    modified the code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 #script to take url and give it's size using curl
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-curl -sI "$1" | grep -i Content-Length | awk '{print $1}'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
