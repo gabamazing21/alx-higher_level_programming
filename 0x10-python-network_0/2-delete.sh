b3b590a9af5f0063950bc185636a0902468c8404
gabriel <gabaamazing21@gmail.com>
2024-09-27 07:55:18 +0100

    Quick commit

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-#delete request
-curl -X DELETE -s "$1" -o -
+
+# Check if a URL is provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# Send a DELETE request using curl and display the body of the response
+curl -sX DELETE "$1"
