f6306b97df9b22b7eed04ad24a9ebbe05cf1901d
gabriel <gabaamazing21@gmail.com>
2024-10-29 21:31:22 +0100

    fix log issue and indentation

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,21 +4,21 @@ const intArgv = [];
 if (argv.length <= 1) {
   console.log(0);
 } else {
-  for (const num of argv){
+  for (const num of argv) {
     intArgv.push(Number.parseInt(num));
   }
   console.log(findSecondInt(intArgv));
 }
 function findSecondInt (arr) {
-    let first = -Infinity;
-    let second = -Infinity;
-    for (const num of arr) {
-        if (num > first) {
-            second = first;
-            first = num;
-        } else if (num > second && num < first) {
-            second = num;
-        }
+  let first = -Infinity;
+  let second = -Infinity;
+  for (const num of arr) {
+    if (num > first) {
+      second = first;
+      first = num;
+    } else if (num > second && num < first) {
+      second = num;
     }
-    return second;
   }
+  return second;
+}
